3e82f4b3457abfa14f9e1e168f3b1c9be411a62a
josexl38 <jmunoz18709@plataforma-utslp.net>
2025-09-02 09:57:16 -0600

    Updated index.html

--- src/config/constants.js
+++
@@ -1,9 +0,0 @@
-export const CAPACITY = 6;
-export const TIMES = ["07:00", "09:00", "18:00"];
-export const DAYS_AHEAD = 21;
-
-export const TEST_USERS = [
-  { email: "usuario@prueba.com", name: "Cliente Demo", role: "user", sessions: 8 },
-  { email: "instructor@prueba.com", name: "Instructor Demo", role: "instructor", sessions: 0 },
-  { email: "admin@prueba.com", name: "Admin Demo", role: "admin", sessions: 0 }
-];
